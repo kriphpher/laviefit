402890dff8801d6dba50b5a8c66b3829db4267a3
Kriphpher <kriffer69@gmail.com>
2023-03-30 22:21:51 -0300

    a

--- laviefit/front/assets/js/tema.js
+++ laviefit/front/assets/js/tema.js
@@ -151,6 +151,22 @@ $(document).ready(function(){
 });
 
 
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 15,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
 
 
 $('.postagemcompleta a').each(function(index, el) {
